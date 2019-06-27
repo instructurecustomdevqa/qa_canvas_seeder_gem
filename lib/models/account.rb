d54ce28a46b7d2ba5149cc8cbfef0009c546d4a7
Trevor Byington <tbyington@instructure.com>
2019-06-27 14:09:55 -0600

    accounts can be randomly generated

--- lib/models/account.rb
+++ lib/models/account.rb
@@ -1,9 +1,29 @@
 class Account < Forgery
-  attr_reader :name, :uid, :sis_id, :description
+  attr_reader :name, :uid, :parent_id, :root_id, :time_zone, :sis_id, :workflow
+
   def to_s
-    string = ""
+    string = "#{@name}, #{@uid}, #{@parent_id}, #{@root_id}, #{@time_zone}, #{@sis_id}, #{@workflow}"
   end
 
-  def self.random
+  def initialize(name, uid, parent, root, time_zone, sis, workflow)
+    @name = name
+    @uid = uid
+    @parent_id = parent
+    @root_id = root
+    @time_zone = time_zone
+    @sis_id = sis
+    @workflow = workflow
+  end
+
+  def self.random (parent_id = 1 , root_id = 1)
+    a = Forgery('name').company_name
+    Account.new(
+      a,
+      "#{a}-#{rand(10000)}",
+      parent_id,
+      root_id,
+      Forgery('time').zone,
+      (10000+rand(10000000)),
+      'active')
   end
 end
--- lib/models/assignment.rb
+++ lib/models/assignment.rb
@@ -1,11 +1,23 @@
 class Assignment < Forgery
+  attr_reader :name, :description, :due_at, :lock_at, :course_id, :assignment_group
 
-  def to_s
-    string = ""
+  def initialize(name, description, due_at, lock_at, course_id, assignment_group)
+    @name = name
+    @description = description
+    @due_at = due_at
+    @lock_at = lock_at
+    @course_id = course_id
+    @assignment_group = assignment_group
   end
 
-  def self.random
-    @name = "#{Forgery('name').last_name}'s take on #{Forgery('lorem_ipsum').title}"
-    @description = Forgery('lorem_ipsum').paragraphs
+  def self.random (course = 1, group = 1)
+    d = Forgery('date').date
+    Account.new(
+      "What #{Forgery('name').job_title} #{Forgery('name').full_name} said about #{Forgery('name').industry}",
+      "#{Forgery('lorem_ipsum').paragraphs}",
+      d,
+      d+10.days,
+      course,
+      group )
   end
 end
